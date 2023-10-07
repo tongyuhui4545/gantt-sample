20fc85bd4ed88ab39bfc5520d4bb9c583823364c
Yuhui <yuhui.tong@voltage-llc.com>
2023-10-07 16:31:29 +0800

    fakingaaaaa

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <header class="header">
+      <div class="tabs">
+        <ul>
+          <li><a>Gantt1</a></li>
+          <li><a>Gantt2</a></li>
+          <li><a>Gantt3</a></li>
+          <li><a>Gantt4</a></li>
+          <li><a>Gantt5</a></li>
+        </ul>
+      </div>
+    </header>
+    <div class="container">
+      <GanttChart class="left-container" :tasks="tasks"></GanttChart>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GanttChart from './components/GanttChart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GanttChart
+  },
+  data() {
+    return {
+      tasks: {
+        data: [
+          { id: 1, text: 'Task #1', start_date: '2020-01-17', duration: 3, progress: 0.6 },
+          { id: 2, text: 'Task #2', start_date: '2020-01-20', duration: 3, progress: 0.4 }
+        ],
+        links: [
+          { id: 1, source: 1, target: 2, type: '0' }
+        ]
+      },
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
-</style>
+
+.header {
+  background-color: #f0f0f0;
+  padding: 10px;
+  display: flex;
+  /* 使用flex布局使子元素水平排列 */
+  justify-content: space-between;
+  /* 水平分散对齐 */
+  align-items: center;
+  /* 垂直居中对齐 */
+}
+
+/* Tabs样式 */
+.tabs ul {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  /* 使用flex布局使选项卡水平排列 */
+}
+
+.tabs li {
+  margin-right: 10px;
+}
+
+.container {
+  height: 100vh;
+  width: 100%;
+}
+
+.left-container {
+  overflow: hidden;
+  position: relative;
+  height: 100%;
+}</style>
---
+++ src/components/GanttChart.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="ganttContainer"></div>
+</template>
+ 
+<script>
+import {gantt} from 'dhtmlx-gantt';
+export default {
+  props: {
+    tasks: {
+      type: Object,
+      default () {
+        return {data: [], links: []}
+      }
+    }
+  },
+ 
+  mounted: function () {
+    gantt.config.date_format = "%Y-%m-%d";
+ 
+    gantt.init(this.$refs.ganttContainer);
+    gantt.parse(this.$props.tasks);
+  }
+}
+</script>
+ 
+<style>
+    @import "~dhtmlx-gantt/codebase/dhtmlxgantt.css";
+</style>
